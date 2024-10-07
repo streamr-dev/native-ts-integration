d7f5c5facade0dc85255d68848bc2baa190c999c
Petri Savolainen <petri.savolainen@spaceify.net>
2024-10-07 13:19:52 +0300

    lock streamr network commit to tuse

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 if [ ! -d "network" ]; then
     git clone https://github.com/streamr-dev/network.git
     cd network
+    git checkout c4e111442
     npm run bootstrap
     cd ..
     npm install
